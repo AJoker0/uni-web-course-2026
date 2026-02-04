f88eb5b80e829e18111d8a25394ed4ea8f86135e
Andrii Titkov <andrii.titkov@gmail.com>
2026-02-04 09:49:09 +0200

    01-Calorie

--- 03-Objects/01-Calorie.js
+++ 03-Objects/01-Calorie.js
@@ -1,5 +1,15 @@
 const calories = (array) => {
+    const calorieObject = {}; //create the place where to store the data
+
+    for (let i = 0; i < array.length -1; i += 2) {
+        const product = array[i]; //take the product name
+        const calories = Number(array[i + 1]); //take the calories and convert to number
+        calorieObject[product] = calories; //add to object
+
+    }
+    return calorieObject;
 
 }
 
-console.log(calories(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']))
+console.log(calories(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
+console.log(calories(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42', 'Bread']));
