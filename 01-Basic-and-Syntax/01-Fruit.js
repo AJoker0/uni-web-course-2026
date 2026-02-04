5b3d00d90b3aa94c7841e08340b8a99c215a302d
Andrii Titkov <andrii.titkov@gmail.com>
2026-02-05 00:40:29 +0200

    Friut 01

---
+++ 01-Basic-and-Syntax/01-Fruit.js
@@ -0,0 +1,12 @@
+const fruitNeed = (system) => {
+    const fruit = system[0];
+    const weight = (system[1] / 1000).toFixed(2);
+    const money = (weight * system[2]).toFixed(2);
+    return `I need ${money} to buy ${weight} kilograms ${fruit}.`;
+    
+}
+
+
+
+console.log(fruitNeed(['orange', 2500, 1.80]));
+console.log(fruitNeed(['apple', 1563, 2.35]));
