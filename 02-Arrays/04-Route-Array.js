936b78c426b8b1acd816710230cc9efdc46faf9e
Andrii Titkov <andrii.titkov@gmail.com>
2026-02-09 13:55:42 +0200

    few tasks more from array section

--- 02-Arrays/04-Route-Array.js
+++ 02-Arrays/04-Route-Array.js
@@ -11,3 +11,20 @@ const rotateArray = (array, rotationsCount) => {
 
 console.log(rotateArray(['1', '2', '3', '4'], 2)); 
 console.log(rotateArray(['Banana', 'Orange', 'Coconut', 'Apple'], 15));
+
+
+
+
+
+/* const rotateArray = (array, rotationsCount) => {
+    for (let i = 0; i < rotationsCount; i++) {
+        let lastElement = array.pop();
+        array.unshift(lastElement);
+        
+
+    }
+    console.log(array.join(' '));
+
+}
+rotateArray(['1', '2', '3', '4'], 2);
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple'], 15); */
