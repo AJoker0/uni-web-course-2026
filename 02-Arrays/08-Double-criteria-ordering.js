9bee2b61b09735f51c8fc578d315eb805290469c
Andrii Titkov <andrii.titkov@gmail.com>
2026-02-11 20:58:32 +0200

    let's go

--- 02-Arrays/08-Double-criteria-ordering.js
+++ 02-Arrays/08-Double-criteria-ordering.js
@@ -1,12 +1,25 @@
-const oderArray = (array) => {
-    return array.sort((a, b) => {
-        if (a.length !== b.length) {
-            return a.length - b.length;
-        }
-        return a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase());
-    })
-}
+/*  const orderArray = (array) => {
+    array.sort((a, b) => {
+        let lengthComparison = a.length - b.length;
+        if (lengthComparison !== 0) {
+            return lengthComparison;
 
+        } else {
+            return a.localeCompare(b);
+        } 
+    });
+    return array;
+} 
+console.log(orderArray(['alpha', 'beta', 'gamma', ]))
+console.log(orderArray(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']))
+console.log(orderArray(['test', 'Deny', 'omen', 'Default']))
+ */
+
+
+const orderArray = (array) => {
+    array.sort((a, b) => a.length - b.length || a.localeCompare(b));
+    return array;
+}
 console.log(orderArray(['alpha', 'beta', 'gamma', ]))
 console.log(orderArray(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']))
 console.log(orderArray(['test', 'Deny', 'omen', 'Default']))
