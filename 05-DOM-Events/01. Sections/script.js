90a70b63b42c8309cb24671a1d5b3b60a618ca75
Andrii Titkov <andrii.titkov@gmail.com>
2026-02-25 15:44:30 +0200

    first and seconds DOM events

--- 05-DOM-Events/01. Sections/script.js
+++ 05-DOM-Events/01. Sections/script.js
@@ -1,3 +1,60 @@
+/* function create(words) {
+   const content = document.getElementById('content');
+
+   // forEachloop through the array of words. word - current word in the array
+   words.forEach(word => {
+      // create a new div element
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+
+      // Filling the content
+      p.textContent = word;
+      p.style.display = 'none'; // Hide the paragraph initially
+
+      // p inside div
+      div.appendChild(p);
+
+      // Add event listener to the div
+      div.addEventListener('click', () => {
+         p.style.display = '';
+
+      });
+      // add to the page
+      content.appendChild(div);
+   });
+
+} */
+
+
+
+
+
+
 function create(words) {
-   console.log('TODO:...');
+   //find the main space where we we add everything
+   let content = document.getElementById('content');
+
+   // go through each word from array
+   for (let i = 0; i < words.length; i++) {
+      // create div and p element
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+
+      // save text in the paragraph and hide it
+      p.textContent = words[i];
+      p.style.display = 'none';
+
+      // put paragraph in the div
+      div.appendChild(p);
+
+      // add event listener to the div
+      div.addEventListener('click', function() {
+         // When clicking, we make a paragraph visible
+         p.style.display = 'block';
+      });
+      // add the div to the page
+      content.appendChild(div);
+   }
+
+
 }
--- 05-DOM-Events/02. Time-Converter/solution.js
+++ 05-DOM-Events/02. Time-Converter/solution.js
@@ -1,4 +1,96 @@
-function attachEventsListeners() {
+/* function attachEventsListeners() {
 
-    console.log('TODO:...');
+    // We collect all the inputs in the object for convenience
+    const inputs = {
+        days: document.getElementById('days'),
+        hours: document.getElementById('hours'),
+        minutes: document.getElementById('minutes'),
+        seconds: document.getElementById('seconds')
+
+    };
+    //  coeficients for converting (relatively 1 day)
+    const ratios = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    };
+
+    // universal function convertation 
+    function convert(value, unit) {
+        // at first we transform the value to days, 
+        const days = value / ratios[unit];
+
+        // then we fill other rows
+        inputs.days.value = days;
+        inputs.hours.value = days * ratios.hours;
+        inputs.minutes.value = days * ratios.minutes;
+        inputs.seconds.value = days * ratios.seconds;
+    }
+    // find buttons and add event listeners
+    // Object.keys(inputs) give us an array ['days', 'hours', 'minutes', 'seconds']
+    Object.keys(inputs).forEach(key => {
+        const btnId = key + 'Btn'; 
+        const btn = document.getElementById(btnId);
+
+        btn.addEventListener('click', function() {
+            // take the value from the input and convert it to number
+            const value = Number(inputs[key].value);
+            convert(value, key);
+        });
+    });
+
+} */
+
+
+
+
+
+
+
+
+function attachEventsListeners() {
+    // We get access to all input fields
+    let daysInput = document.getElementById('days');
+    let hoursInput = document.getElementById('hours');
+    let minutesInput = document.getElementById('minutes');
+    let secondsInput = document.getElementById('seconds');
+
+    // We get access all buttons 
+    let daysBtn = document.getElementById('daysBtn');
+    let hoursBtn = document.getElementById('hoursBtn');
+    let minutesBtn = document.getElementById('minutesBtn');
+    let secondsBtn = document.getElementById('secondsBtn');
+
+    // logic for button DAYS
+    daysBtn.addEventListener('click', function() {
+        let days = Number(daysInput.value);
+        hoursInput.value = days * 24;
+        minutesInput.value = days * 1440;
+        secondsInput.value = days * 86400;
+
+    });
+
+    //logic for button HOURS
+    hoursBtn.addEventListener('click', function() {
+        let hours = Number(hoursInput.value);
+        daysInput.value = hours / 24;
+        minutesInput.value = hours * 60;
+        secondsInput.value = hours * 3600;
+
+    });
+    //logic for button MINUTES
+    minutesBtn.addEventListener('click', function() {
+        let minutes = Number(minutesInput.value);
+        daysInput.value = minutes / 1440;
+        hoursInput.value = minutes / 60;
+        secondsInput.value = minutes * 60;
+    });
+    //logic for button SECONDS
+    secondsBtn.addEventListener('click', function() {
+        let seconds = Number(secondsInput.value);
+        daysInput.value = seconds / 86400;
+        hoursInput.value = seconds / 3600;
+        minutesInput.value = seconds / 60;
+    });
 }
